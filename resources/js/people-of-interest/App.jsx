55c81be3e7cb8f6b09b489c989bdb40ef654e8ab
Mmarkev <monika@data4you.cz>
2024-03-13 10:35:54 +0100

    register added

--- resources/js/people-of-interest/App.jsx
+++ resources/js/people-of-interest/App.jsx
@@ -1,13 +1,35 @@
 import LeftMenu from './LeftMenu';
 import Main from './Main';
 import './App.scss';
+import { BrowserRouter } from 'react-router-dom';
+import UserContext from './UserContext';
+import { useEffect, useState } from 'react';
 
 export default function App() {
 
+    const [user, setUser] = useState(null)
+
+    const getUser = async () => {
+        const response = await fetch('/api/user');
+
+        if (response.status == 200) {
+            const data = await response.json();
+            setUser(data)
+        }
+    }
+
+    useEffect(() => {
+        getUser();
+    }, [])
+
     return (
         <>
-            <LeftMenu/>
-            <Main />
+            <UserContext.Provider value={{user, setUser, getUser}}>
+                <BrowserRouter>
+                    <LeftMenu/>
+                    <Main />
+                </BrowserRouter>
+            </UserContext.Provider>
         </>
     )
 }
--- resources/js/people-of-interest/Main.jsx
+++ resources/js/people-of-interest/Main.jsx
@@ -1,12 +1,25 @@
 import People from "./People"
+import { Routes, Route } from 'react-router-dom';
+import Register from "./Register";
+import { useContext } from "react";
+import UserContext from "./UserContext";
 
 export default function Main() {
-
+    const { user } = useContext(UserContext)
     return (
         <main className="main">
 
             <div className="main__content">
-                <People />
+                <Routes>
+                    <Route path="/" element={
+                        user ? <h1>Welcome to MI6, {user.name}</h1>
+                        :
+                         <h1>Welcome to MI6</h1>
+                    } />
+                    <Route path="/people-of-interest" element={ <People /> } />
+                    <Route path="/register" element={ <Register /> } />
+
+                </Routes>
             </div>
 
         </main>
